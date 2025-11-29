6dede2848e5fa9159b33370295ab7654e6831ea8
eseegm97 <eseegm97@gmail.com>
2025-11-29 16:04:06 -0600

    expense_tracker

---
+++ expense_tracker/Expense.java
@@ -0,0 +1,30 @@
+package expense_tracker;
+
+public class Expense {
+    private String date;
+    private String description;
+    private double amount;
+
+    public Expense(String date, String description, double amount) {
+        this.date = date;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Expense(date=" + date + ", description=" + description + ", amount=" + amount + ")";
+    }
+}
